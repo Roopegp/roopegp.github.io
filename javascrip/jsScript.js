677158ae6d1de232bb6cbddd540dcc99b8f60a49
Roopegp <112859697+Roopegp@users.noreply.github.com>
2022-10-05 12:57:00 +0300

    y

---
+++ javascrip/jsScript.js
@@ -0,0 +1,11 @@
+
+function mDown(obj) {
+  obj.style.backgroundColor = "#1ec5e5";
+  obj.innerHTML = "Release Me";
+}
+
+function mUp(obj) {
+  obj.style.backgroundColor="#D94A38";
+  obj.innerHTML="Thank You";
+}
+	
