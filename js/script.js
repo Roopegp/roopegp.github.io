8039bd4def52c9219ee7b333060a59426c3bfa30
Roopegp <112859697+Roopegp@users.noreply.github.com>
2023-03-17 09:22:36 +0200

    slider

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const slides =document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+slides.forEach(function(slide,index) {
+    slide.style.left=  `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener("click",function() {
+    counter++;
+    cycle();
+});
+prevBtn.addEventListener("click",function() {
+    counter--;
+    cycle();
+});
+
+function cycle() {
+    if(counter == slides.length) counter =0 ;
+    if(counter < 0) counter =slides.length -1;
+    slides.forEach(function(slide) {
+        slide.style.transform = `translateX(-${counter * 100}%)`
+    })
+}
