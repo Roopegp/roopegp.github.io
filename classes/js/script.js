4888ea33913fe143efe834786e025a5ce861471b
Roopegp <112859697+Roopegp@users.noreply.github.com>
2023-04-28 10:50:31 +0300

    Radiobuttons

--- classes/js/script.js
+++ classes/js/script.js
@@ -5,20 +5,25 @@ class bill {
         this.type = type;
    }
    isEmpty() {
-       return hinta ==0 && pvm == " " && this.type == " ";
+       return hinta == 0 && pvm ===""&& this.type == "";
    }
    print(dom) {
         dom.innerHTML    += "<h1>" +this.hinta +"€ | "+ this.pvm + " | " +this.type +" </h1>";
    }
 
 }
+function getRBtn(rbName) {
+    let opt = document.querySelector(`input[name=${rbName}]:checked`);
+    return opt = (opt != null) ? opt.value : 'meno';  
+  }
+
 var billArr = new Array();
 var hinta = document.querySelector(".js-hinta");
 var pvm = document.querySelector(".js-pvm")
-var menoTulo = document.querySelector(".js-menoOrTulo")
 var acbod = document.querySelector(".accordion-body")
 var submit = document.querySelector(".js-submit").addEventListener("click",function(e) {
-    var type = menoTulo.value;
+    var type = getRBtn("type");
+
     var newBill = new bill(Number(hinta.value),pvm.value,type)
     if(!newBill.isEmpty()) {
         billArr.push(newBill);
