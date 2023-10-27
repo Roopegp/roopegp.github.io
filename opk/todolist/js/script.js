6acdf257d8b10219fa844d6fbf8defdc9d0ecb4c
Roopegp <112859697+Roopegp@users.noreply.github.com>
2023-10-27 10:41:39 +0300

    fixed bug loading empty storage

--- opk/todolist/js/script.js
+++ opk/todolist/js/script.js
@@ -4,27 +4,27 @@ var btndel = document.getElementById("btndel");
 var list = document.getElementById("list");
 var currentItems = [];
 load();
-btn.addEventListener("click",doSomething);
-btndel.addEventListener("click",yeetChild);
+btn.addEventListener("click", doSomething);
+btndel.addEventListener("click", yeetChild);
 function yeetChild() {
     list.removeChild(list.lastElementChild);
     var index = currentItems.findIndex(e => e == this.parentNode.innerText);
-    currentItems.splice(index,1);
+    currentItems.splice(index, 1);
     console.log(currentItems);
-    if (typeof(Storage) !== "undefined") { 
+    if (typeof (Storage) !== "undefined") {
         localStorage.removeItem("items");
-        localStorage.setItem("items",JSON.stringify(currentItems));
+        localStorage.setItem("items", JSON.stringify(currentItems));
     }
 }
 function yeetCurrentChild() {
-    
+
     this.parentNode.remove();
     var index = currentItems.findIndex(e => e == this.parentNode.innerText);
-    currentItems.splice(index,1);
+    currentItems.splice(index, 1);
     console.log(currentItems);
-    if (typeof(Storage) !== "undefined") { 
+    if (typeof (Storage) !== "undefined") {
         localStorage.removeItem("items");
-        localStorage.setItem("items",JSON.stringify(currentItems));
+        localStorage.setItem("items", JSON.stringify(currentItems));
     }
 }
 
@@ -32,32 +32,37 @@ function yeetCurrentChild() {
 
 function doSomething(e) {
     let btnChild = document.createElement("button");
-    btnChild.setAttribute("class","btn-danger btn btn-lg rounded-1")
+    btnChild.setAttribute("class", "btn-danger btn btn-lg rounded-1")
     btnChild.innerHTML = "Poista";
-    btnChild.addEventListener("click",yeetChild);
+    btnChild.addEventListener("click", yeetChild);
     let child = document.createElement("li");
-    child.setAttribute("class","list-group-item");
+    child.setAttribute("class", "list-group-item");
     child.innerHTML = txtField.value;
     child.appendChild(btnChild);
     list.appendChild(child);
     currentItems.push(txtField.value);
-    if (typeof(Storage) !== "undefined") { 
-        localStorage.setItem("items",JSON.stringify(currentItems));
+    if (typeof (Storage) !== "undefined") {
+        localStorage.setItem("items", JSON.stringify(currentItems));
     }
-    
+
 }
 
 function load() {
-    currentItems = JSON.parse(localStorage.getItem("items"));
-    currentItems.forEach(e =>{
-        let btnChild = document.createElement("button");
-        btnChild.setAttribute("class","btn-danger btn btn-lg rounded-1")
-        btnChild.innerHTML = "Poista";
-        btnChild.addEventListener("click",yeetCurrentChild);
-        let child = document.createElement("li");
-        child.setAttribute("class","list-group-item");
-        child.innerHTML = e;
-        child.appendChild(btnChild);
-        list.appendChild(child);
-    });
+    if (typeof (Storage) !== "undefined") {
+        var currentItemsTemp = JSON.parse(localStorage.getItem("items"));
+        if (currentItemsTemp) {
+            currentItemsTemp.forEach(e => {
+                let btnChild = document.createElement("button");
+                btnChild.setAttribute("class", "btn-danger btn btn-lg rounded-1")
+                btnChild.innerHTML = "Poista";
+                btnChild.addEventListener("click", yeetCurrentChild);
+                let child = document.createElement("li");
+                child.setAttribute("class", "list-group-item");
+                child.innerHTML = e;
+                child.appendChild(btnChild);
+                list.appendChild(child);
+            });
+            currentItems = currentItemsTemp;
+        }
+    }
 }
